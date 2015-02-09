15583592f7f131737a399beac67131e0294328ce
Matthew Bunday <zenceph@gmail.com>
2015-02-09 14:55:39 -0500

    Update arena with new logic.

--- arena.rb
+++ arena.rb
@@ -1,29 +1,28 @@
-class Player
-  attr_reader :glyph
-  attr_accessor :x, :y
-
-  def initialize(glyph, x, y)
-    @glyph, @x, @y = glyph, x, y
-  end
-
-  def at?(x, y)
-    @x == x && @y == y
-  end
-end
+require 'pry'
 
 class Arena
-  def initialize(width, height, player)
+  def initialize(width, height, player, things)
     @height, @width = height, width
     @player = player
+    @things = things
+  end
+
+  def item_at(x, y)
+    @things.select {|t| t.at?(x, y)}.first
+  end
+
+  def lose_item(item)
+    @things.delete(item)
   end
 
   def to_s
     (0..@height).map do |y|
       (0..@width).map do |x|
-        case true
-        when @player.at?(x, y)
+        if @player.at?(x, y)
           @player.glyph
-        when x == 0 || x == @width || y == 0 || y == @height
+        elsif item_at(x, y)
+          item_at(x, y).glyph
+        elsif x == 0 || x == @width || y == 0 || y == @height
           '#'
         else
           '.'
@@ -33,53 +32,3 @@ class Arena
   end
 end
 
-class Game
-  def initialize
-    @player = Player.new('@', 10, 10)
-    @arena = Arena.new(20, 20, @player)
-  end
-
-  def play!
-    loop do
-      reset_screen!
-      puts @arena
-      c = get_char
-      
-      case c
-      when 'l'
-        @player.x += 1
-      when 'h'
-        @player.x -= 1
-      when 'j'
-        @player.y += 1
-      when 'k'
-        @player.y -= 1
-      end
-    end
-  end
-
-  def clear_screen!
-    print "\e[2J"
-  end
-
-  def move_to_home!
-    print "\e[H"
-  end
-
-  def reset_screen!
-    clear_screen!
-    move_to_home!
-  end
-
-  def get_char
-    state = `stty -g`
-    `stty raw -echo -icanon isig`
-
-    STDIN.getc.chr
-  ensure
-    `stty #{state}`
-  end
-end
-
-g = Game.new
-g.play!
